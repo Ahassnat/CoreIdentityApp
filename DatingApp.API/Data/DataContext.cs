d3dfd57055151ed0d358cafa1afa32bfc5985146
Abdallah <alhassnatabdallah@gmail.com>
2019-12-24 03:22:13 +0200

    Configuring the DataContext for Identity

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -1,14 +1,18 @@
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext:DbContext
+    public class DataContext:IdentityDbContext<User, Role, int, IdentityUserClaim<int>,
+                                                UserRole,IdentityUserLogin<int>, IdentityRoleClaim<int>,
+                                                IdentityUserToken<int>>
     {
         public DataContext(DbContextOptions<DataContext> Options) : base(Options){}
         
         public DbSet<Value> Values { get; set; }
-        public DbSet<User> Users { get; set; }
+
 
         public DbSet<Photo> Photos { get; set; }
         public DbSet<Like> Likes { get; set; }
@@ -18,6 +22,16 @@ namespace DatingApp.API.Data
         // we use this function with the (Many-to-Many) Relationship
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder); // configure the schema needed for Identity
+            
+            builder.Entity<UserRole>(userRole => 
+            {
+                userRole.HasKey(ur => new{ur.UserId, ur.RoleId});
+                userRole.HasOne(ur => ur.Role).WithMany(r => r.UserRoles).HasForeignKey(ur =>ur.RoleId).IsRequired();
+                userRole.HasOne(ur => ur.User).WithMany(r => r.UserRoles).HasForeignKey(ur =>ur.UserId).IsRequired();
+            });
+
+
             builder.Entity<Like>()
                 .HasKey(k => new {k.LikerId, k.LikeeId}); // to make primary key
 
